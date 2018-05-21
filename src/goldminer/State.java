0d891a0f889290d0319c806589a9a0149fcf39d3
Fugoes <fugoes.qa@gmail.com>
2018-05-21 20:57:00 +0800

    add Pocket

--- src/goldminer/State.java
+++ src/goldminer/State.java
@@ -22,6 +22,7 @@ public class State {
         this.entities.add(new GoldMax(1000, 500));
         this.entities.add(new GoldMid(1000, 700));
         this.entities.add(new GoldMin(1000, 900));
+        this.entities.add(new Pocket(1300, 900));
     }
 
     public void move(int playerID, long time) {
