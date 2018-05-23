a9f829dfc23f8beea3f9f6ea122086b7737909f1
Fugoes <fugoes.qa@gmail.com>
2018-05-23 14:18:16 +0800

    connections

--- src/goldminer/Connections.java
+++ src/goldminer/Connections.java
@@ -9,14 +9,30 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Calendar;
 import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class Connections {
-    public interface ConnectionBase {
-        void send(String string);
+    public abstract class ConnectionBase {
+        boolean isMaster;
+        BiConsumer<Integer, Long> receiveMoveCallback;
+        Runnable gameStartFunction;
+        Supplier<String> stateSupplier;
+        Consumer<String> stateConsumer;
 
-        boolean isUp();
+        ConnectionBase(boolean isMaster,
+                       BiConsumer<Integer, Long> receiveMoveCallback,
+                       Runnable gameStartFunction,
+                       Supplier<String> stateSupplier,
+                       Consumer<String> stateConsumer) {
+            this.isMaster = isMaster;
+            this.receiveMoveCallback = receiveMoveCallback;
+            this.gameStartFunction = gameStartFunction;
+            this.stateSupplier = stateSupplier;
+            this.stateConsumer = stateConsumer;
+        }
 
-        boolean waitForUp(int timeout);
+        abstract void sendMove(int playerID, long time);
     }
 
     public static class TCPServer implements ConnectionBase {
